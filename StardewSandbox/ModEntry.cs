c631551e4a75baa9e6e75a9b3844d1460babc979
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-06-09 15:04:15 -0500

    Basic template

---
+++ StardewSandbox/ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+
+namespace StardewSandbox
+{
+    public class ModEntry : Mod
+    {
+        // Shared static helpers
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+        internal static Multiplayer multiplayer;
+
+        public override void Entry(IModHelper helper)
+        {
+            // Set up the monitor, helper and multiplayer
+            monitor = Monitor;
+            modHelper = helper;
+            multiplayer = helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
+
+            try
+            {
+                var harmony = new Harmony(this.ModManifest.UniqueID);
+
+                // Apply patches
+            }
+            catch (Exception e)
+            {
+                Monitor.Log($"Issue with Harmony patching: {e}", LogLevel.Error);
+                return;
+            }
+
+            // Hook into the required events
+        }
+    }
+}
